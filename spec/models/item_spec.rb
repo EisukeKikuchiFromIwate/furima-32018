b218646188113fdd10a5031f131df545c98af80b
EisukeKikuchiFromIwate <73880312+EisukeKikuchiFromIwate@users.noreply.github.com>
2020-12-10 19:20:06 +0900

    修正2

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -95,12 +95,12 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include('Price は半角数字を使用してください')
     end
     it '価格が299以下の場合は登録できない' do
-      @item.price = '200'
+      @item.price = 200
       @item.valid?
       expect(@item.errors.full_messages).to include('Price is not included in the list')
     end
     it '価格が10_000_000以上の場合は登録できない' do
-      @item.price = '10000000'
+      @item.price = 10000000
       @item.valid?
       expect(@item.errors.full_messages).to include('Price is not included in the list')
     end
